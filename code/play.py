8b15b9514dbc18abff4caef5a3a2138f4fe543b6
AndaChain <32815766+AndaChain@users.noreply.github.com>
2020-10-21 17:39:38 +0700

    Update play.py

--- code/play.py
+++ code/play.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python3
-"""Play a sine signal."""
+"""Play a signal."""
 import numpy as np
 import sounddevice as sd
 from wave import make_sound as make
 import time
-import pygame
-
-pygame.init()
-
-#camera wide & height depends on screen wide & height
-scr_w, scr_h = 640, 480
-
-screen = pygame.display.set_mode( (scr_w,scr_h) )
-surface = pygame.Surface( screen.get_size(), pygame.SRCALPHA )
 
 class play(make):
     def __init__(self, sps=44100 , freq_hz=1.00, duration_s=5, atten=0.5):
+        super().__init__(sps=44100 , freq_hz=1.00, duration_s=5, atten=0.5)
+
         # Samples per second
         self.sps = sps #44100 CD format
 
@@ -30,79 +23,78 @@ class play(make):
 
         self.start_idx = 0
 
-        self.close = {"sine":(0, False), "square":(0, False), "triangle":(0, False), "sawtooth":(0, False)}
+        self.close = {"sine":[0, 0, False], "square":[1, 0, False], "triangle":[2, 0, False], "sawtooth":[3, 0, False]}
 
+        self.note = {"B":2, "Bb":1, "A":0, "G#":-1, "G":-2, "F#":-3, "F":-4, "E":-5, "D#":-8, "D":-7, "C#":-8, "C":-9}
         self.running = True
+
+        self.combine_wave = []
+
+        self.form = "sine"
+        self.N = "A"
+        self.num = 1
+        self.state = self.close[self.form][2]
+        self.waveform = 0
+
     def callback(self, data=[], frames=0, time=0, status=0):
+        
         self.t = (self.start_idx + np.arange(frames)) #/ self.sps
         self.t = self.t.reshape(-1, 1)
-        self.waveform = self.Oscillators(0, self.close["sine"][0], self.close["sine"][1]) + self.Oscillators(1, self.close["square"][0], self.close["square"][1]) + self.Oscillators(2, self.close["triangle"][0], self.close["triangle"][1]) + self.Oscillators(3, self.close["sawtooth"][0], self.close["sawtooth"][1])
+        self.waveform = 0
+
+        for i in range(self.num):
+            self.waveform += self.Oscillators(  self.close[self.form][0],
+                                            self.close[self.form][1]+i*8,
+                                            self.close[self.form][2]  )
+
         data[:] = self.atten * (self.waveform)
         self.start_idx += frames
 
-    def sound(self):
+    def play_sound(self):
         with sd.OutputStream(channels=1, callback=self.callback, samplerate=self.sps):
             while self.running:
-                #sd.sleep(1)
+                sd.sleep(1)
                 try:
-                    temp = [i for i in input("Enter: ").split(" ")]
-                    n = int(temp[1])
-                    if(temp[2] == "single"):
-                        if(temp[0] == "quiet"):
-                            temp = None
-                            self.set_wave(  (0,False), (0,False), (0,False), (0,False)  )
-                        elif(temp[0] == "sine"):
-                            temp = None
-                            self.set_wave(  (n,True), (n,False), (n,False), (n,False)  )
-                        elif(temp[0] == "squ"):
-                            temp = None
-                            self.set_wave(  (n,False), (n,True), (n,False), (n,False)  )
-                        elif(temp[0] == "tri"):
-                            temp = None
-                            self.set_wave(  (n,False), (n,False), (n,True), (n,False)  )
-                        elif(temp[0] == "saw"):
-                            temp = None
-                            self.set_wave(  (n,False), (n,False), (n,False), (n,True)  )
-                        elif(temp[0] == "stop"):
-                            break
-                    elif(temp[2] == "combine"):
-                        if(temp[0] == "sine"):
-                            temp = None
-                            self.close["sine"] = (n,True)
-                        elif(temp[0] == "squ"):
-                            temp = None
-                            self.close["square"] = (n,True)
-                        elif(temp[0] == "tri"):
-                            temp = None
-                            self.close["triangle"] = (n,True)
-                        elif(temp[0] == "saw"):
-                            temp = None
-                            self.close["sawtooth"] = (n,True)
+                    temp = [i for i in input(f"Enter {self.form} {self.N}:").split(" ")]
+                    mode = temp[0]
+                    self.N = temp[1]
+                    self.num = int(temp[2])
+                    print( self.freq_hz*(2**(self.note[self.N]/12)) )
 
+                    if(mode == "sine"):
+                        self.form = "sine"
+                        self.close[self.form][1] = self.note[self.N]
+                        self.close[self.form][2] = True
+                    elif(mode == "squ"):
+                        self.form = "square"
+                        self.close[self.form][1] = self.note[self.N]
+                        self.close[self.form][2] = True
+                    elif(mode == "tri"):
+                        self.form = "triangle"
+                        self.close[self.form][1] = self.note[self.N]
+                        self.close[self.form][2] = True
+                    elif(mode == "saw"):
+                        self.form = "sawtooth"
+                        self.close[self.form][1] = self.note[self.N]
+                        self.close[self.form][2] = True
+                    self.state = temp
+                    
                 except IndexError:
-                    if(temp[0] == "quiet"):
-                        temp = None
-                        self.set_wave(  (0,False), (0,False), (0,False), (0,False)  )
-                        print("quiet")
-                    elif(temp[0] == "stop"):
+                    if(temp[0] == "||"):
+                        self.close[self.form][2] = False
+                        print(self.form,5)
+                    elif(temp[0] == ">"):
+                        self.close[self.form][2] = True
+                        print(self.form)
+                    elif(mode == "plot"):
+                        self.plot_wave()
+                    elif(temp[0] == 'X'):
+                        self.close[self.form][2] = False
                         break
-                except ValueError:
-                    print(temp,"ValueError")
-    
-    def set_wave(self, sine, square, triangle, sawtooth):
-        self.close["sine"] = sine
-        self.close["square"] = square
-        self.close["triangle"] = triangle
-        self.close["sawtooth"] = sawtooth
-
-    def play_sound(self, sine, square, triangle, sawtooth):
-        self.close["sine"] = sine
-        self.close["square"] = square
-        self.close["triangle"] = triangle
-        self.close["sawtooth"] = sawtooth
-        self.sound()
-
-    
+                    print(temp[0],"IndexError")
+                except KeyError:
+                    print(self.state)
+                    
 if __name__ == "__main__":
-    wave = play(sps=44100 , freq_hz=440.00, duration_s=5, atten=0.1)
-    wave.play_sound(  (0,False), (0,False), (0,False), (0,False)  )
+    wave_obj = play(sps=44100 , freq_hz=440.00, duration_s=5, atten=1)
+    wave_obj.play_sound()
